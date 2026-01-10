b6c5dbf919dfb3e32c53f81b9f408e41d0422df1
Isaac Zhang <isaac90630@gmail.com>
2026-01-09 22:58:23 -0500

    Updated tests

--- test/helpers/fixtures.js
+++ test/helpers/fixtures.js
@@ -1,7 +1,25 @@
 import { readFile } from 'node:fs/promises';
 
+const fixtureCache = new Map();
+
+function cloneFixture(data) {
+  if (typeof structuredClone === 'function') {
+    return structuredClone(data);
+  }
+  return JSON.parse(JSON.stringify(data));
+}
+
 export async function readFixture(name) {
+  if (fixtureCache.has(name)) {
+    return cloneFixture(fixtureCache.get(name));
+  }
+
   const url = new URL(`../fixtures/${name}`, import.meta.url);
-  const data = await readFile(url, 'utf8');
-  return JSON.parse(data);
+  const data = JSON.parse(await readFile(url, 'utf8'));
+  fixtureCache.set(name, data);
+  return cloneFixture(data);
+}
+
+export function clearFixtureCache() {
+  fixtureCache.clear();
 }
